a4a6e3a337cc1a477fc6943e4f9de1d9d921a101
Sohail Gsais <Gsais.sohail9@gmail.com>
2022-01-23 19:46:37 +0000

    added api logger

--- db-controllers.js
+++ db-controllers.js
@@ -1,56 +1,66 @@
 const { MongoClient, ObjectId } = require("mongodb");
 let db;
-(async function connectDb() {
+async function connectDb() {
   const uri =
     "mongodb+srv://root:rootpass123@afterschool-app.j4rka.mongodb.net/test";
   const client = new MongoClient(uri);
   try {
     // Connect to the MongoDB cluster
-    db =  (await client.connect()).db('school-app');
-    console.log("Connected to MongoDB cluster ", db.databaseName);
-    return db
+    db = (await client.connect()).db("school-app");
+    return db;
   } catch (e) {
     console.error(e);
   }
-}())
+}
 
-async function storeOrderInDb(req,res,next){
-    const orderDetails = req.body;
-    const lessonId = orderDetails["_id"]["$oid"];
-    const { subject, location, price, space, image } = orderDetails;
-    const results = await db
-      .collection("orders")
-      .find({ lessonId: lessonId })
-      .toArray();
-
-    if (results.length === 1) {
-      db.collection("orders").updateOne({lessonId: lessonId},{$inc:{quantity:1}})
-      res.status(200).send("updated quantity");
-    } else {
-      const order = {
-        lessonId,
-        subject,
-        location,
-        price,
-        space,
-        image,
-        quantity: 1,
-      };
-      db.collection("orders").insertOne(order, (err, res) => {
-        if (err) throw err;
-      });
-      res.status(200).send(order);
+async function storeOrderInDb(req, res, next) {
+  const {
+    name,
+    phone,
+    space,
+    _id: { $oid },
+  } = req.body;
+  req.collection.insertOne(
+    { name, phone, space, lessonId: $oid },
+    (err, result) => {
+      if (err) throw err;
+      res.send({ orderId: result.insertedId });
     }
-
+  );
 }
 
-async function getLessonsFromDb(req,res,next){
-   req.collection.find({}).toArray((err, result) => {
-     if (err) console.log(err);
-     res.send(result);
-   });
+async function getLessonsFromDb(req, res, next) {
+  req.collection.find({}).toArray((err, result) => {
+    if (err) console.log(err);
+    res.send(result);
+  });
 }
 
+async function updateLessonSpaces(req, res, next) {
+  req.collection.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: { space: req.body.spaces } },
+    { safe: true, multi: false },
+    (err, result) => {
+      if (err) throw err;
+      res.status(200).send("updated spaces");
+    }
+  );
+}
 
-
-module.exports = { storeOrderInDb, getLessonsFromDb };
+async function deleteLessonFromDb(req, res, next) {
+  req.collection.deleteOne(
+    { _id: new ObjectId(req.params.id) },
+    (err, results) => {
+      if (err) throw err;
+      res.status(200).send("lesson deleted");
+    }
+  );
+}
+module.exports = {
+  deleteLessonFromDb,
+  connectDb,
+  updateLessonSpaces,
+  storeOrderInDb,
+  getLessonsFromDb,
+};
