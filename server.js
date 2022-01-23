87c06e4a6061dc84d9f11ff89e3f9e2e1eb52d45
Sohail Gsais <Gsais.sohail9@gmail.com>
2022-01-23 19:46:12 +0000

    added put and delete

--- server.js
+++ server.js
@@ -4,26 +4,33 @@ const port = 3000;
 const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { connectDb, storeOrderInDb, getLessonsFromDb } = require("./db-controllers");
+const { connectDb, storeOrderInDb, getLessonsFromDb, updateLessonSpaces, deleteLessonFromDb } = require("./db-controllers");
 // app.use(bodyParser());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// connect to mongodb database
-
+// api logger middleware
+const myLogger =  (req, res, next) =>{
+  console.log(`API REQUEST : => (${res.statusCode}) ${req.method} ${req.url}`);
+  next();
+};
+app.use(myLogger);
 app.param("name", async (req, res, next, collectionName) => {
   const db = await connectDb();
   req.collection = db.collection(collectionName);
   return next();
 });
+
+
 // GET /collection/lessons returns list of lessons from mongodb
 app.get("/collection/:name", getLessonsFromDb);
 // POST /orders
-app.post("/orders", storeOrderInDb );
+app.post("/collection/:name", storeOrderInDb);
+// update lesson spaces in mongodb
+app.put("/collection/:name/:id",updateLessonSpaces);
+// delete mongodb document
+app.delete("/collection/:name/:id", deleteLessonFromDb);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
